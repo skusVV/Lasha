d0c61ea28eb801e7cb55197566802ce9bc445914
lashachubo <lashachubinidz3@gmail.com>
2024-03-13 18:45:18 +0400

    removed priuses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,16 +53,6 @@ const cars = [
     labels: ['Minivan', 'Petrol']
   },
   {
-    img: 'https://static.my.ge/myauto/photos/3/0/8/3/1/thumbs/102138035_1.jpg?v=1',
-    city: 'On a way',
-    year: 2014,
-    madeBy: 'Toyota',
-    model: 'Prius',
-    price: 17300,
-    currency: '$',
-    labels: ['Sedan', 'Hybrid']
-  },
-  {
     img: 'https://static.my.ge/myauto/photos/1/8/5/2/1/thumbs/101125811_1.jpg?v=6',
     city: 'Tbilisi',
     year: 2017,
@@ -71,17 +61,7 @@ const cars = [
     price: 30800,
     currency: '$',
     labels: ['Minivan', 'Petrol']
-  },
-  {
-    img: 'https://static.my.ge/myauto/photos/3/0/8/3/1/thumbs/102138035_1.jpg?v=1',
-    city: 'On a way',
-    year: 2014,
-    madeBy: 'Toyota',
-    model: 'Prius',
-    price: 17300,
-    currency: '$',
-    labels: ['Sedan', 'Hybrid']
-  },
+  }
 ];
 
 
